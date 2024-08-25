814c9f14d93208db80fb3495166b89b714558203
santi <santiagofb932@gmail.com>
2024-08-25 20:25:05 -0300

    before change to nav link styles

--- src/app/shared/components/link/link.component.ts
+++ src/app/shared/components/link/link.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'pf-link',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './link.component.html',
   styleUrl: './link.component.scss'
 })
-export class LinkComponent {
+export class LinkComponent implements OnInit{
 
+  @Input() set link(value: string)
+  {
+    this._link = value ? value : '/';
+  }
+  _link?: string;
+
+  ngOnInit(): void {
+    console.log(this._link);
+  }
 }
